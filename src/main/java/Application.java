acc5701059b7a1163c9f00caa7a50cf363d37527
Andrii Pyvovarov <andro999b@gmail.com>
2020-11-10 14:57:17 +0200

    ask reply pattern

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,16 @@
 import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.AskPattern;
 import data.Plan;
 import logic.actors.Root;
 import logic.readers.PlanReader;
 import logic.readers.PlanReaderImpl;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
@@ -13,6 +18,20 @@ public class Application {
         InputStream inputStream = Application.class.getClassLoader().getResourceAsStream("plan.txt");
         Plan plan = reader.read(inputStream);
 
-        ActorSystem.create(Root.create(plan), "root");
+        var system = ActorSystem.create(Root.create(), "root");
+
+        CompletionStage<Root.Reply> result = AskPattern.ask(
+                system,
+                reply -> new Root.Run(plan, reply),
+                Duration.ofSeconds(3),
+                system.scheduler()
+        );
+
+        result.whenComplete((reply, failure) -> {
+            if (reply instanceof Root.Mowers)
+                log.info("Result: {}", ((Root.Mowers) reply).getMowers());
+            else
+                log.error("Computation filed. ", failure);
+        });
     }
 }
--- src/main/java/logic/actors/Root.java
+++ src/main/java/logic/actors/Root.java
@@ -1,10 +1,10 @@
 package logic.actors;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Routers;
-import com.sun.jdi.Bootstrap;
 import data.Mower;
 import data.Plan;
 import lombok.Value;
@@ -15,12 +15,18 @@ import java.util.TreeSet;
 
 public final class Root {
 
-    interface Command {
+    public interface Command { }
+    public interface Reply { }
+
+    @Value
+    public static class Mowers implements Reply {
+        Collection<Mower> mowers;
     }
 
     @Value
-    public static class Init implements Root.Command {
+    public static class Run implements Command {
         Plan plan;
+        ActorRef<Reply> replyTo;
     }
 
     @Value
@@ -34,36 +40,42 @@ public final class Root {
         this.context = context;
     }
 
-    public static Behavior<Command> create(Plan plan) {
-        return Behaviors.setup(ctx -> new Root(ctx).init(plan));
+    public static Behavior<Command> create() {
+        return Behaviors.setup(ctx -> new Root(ctx).run());
     }
 
-    private Behavior<Command> init(Plan plan) {
-        var intersectionChecker = context.spawn(IntersectionChecker.create(), "intersectionChecker");
-        var router =
-                Routers.pool(
-                        plan.getScripts().size(),
-                        ScriptProcessor.create(
-                                plan.getLawn(),
-                                intersectionChecker,
-                                context.getSelf()
-                        )
-                ).withRoundRobinRouting();
+    private Behavior<Command> run() {
+        return Behaviors
+                .receive(Command.class)
+                .onMessage(Run.class, (init) -> {
+                    var plan = init.plan;
+                    var intersectionChecker = context.spawn(IntersectionChecker.create(), "intersectionChecker");
+                    var router =
+                            Routers.pool(
+                                    plan.getScripts().size(),
+                                    ScriptProcessor.create(
+                                            plan.getLawn(),
+                                            intersectionChecker,
+                                            context.getSelf()
+                                    )
+                            ).withRoundRobinRouting();
 
-        var scriptProcessor = context.spawn(router, "scriptProcessor");
+                    var scriptProcessor = context.spawn(router, "scriptProcessor");
 
-        plan.getScripts()
-                .stream()
-                .map(ScriptProcessor.RunScript::new)
-                .forEach(scriptProcessor::tell);
+                    plan.getScripts()
+                            .stream()
+                            .map(ScriptProcessor.RunScript::new)
+                            .forEach(scriptProcessor::tell);
 
-        return awaitDone(new TreeSet<>(Comparator.comparingInt(Mower::getId)), plan.getScripts().size());
+                    return awaitDone(new TreeSet<>(Comparator.comparingInt(Mower::getId)), plan.getScripts().size(), init.replyTo);
+                })
+                .build();
     }
 
-    private Behavior<Command> awaitDone(Collection<Mower> doneMowers, int mowersLeft) {
+    private Behavior<Command> awaitDone(Collection<Mower> doneMowers, int mowersLeft, ActorRef<Reply> replyTo) {
 
         if(mowersLeft == 0) {
-            context.getLog().info("{}", doneMowers);
+            replyTo.tell(new Mowers(doneMowers));
             return Behaviors.stopped();
         }
 
@@ -71,7 +83,7 @@ public final class Root {
                 .receive(Command.class)
                 .onMessage(Done.class, done -> {
                     doneMowers.add(done.mower);
-                    return awaitDone(doneMowers, mowersLeft - 1);
+                    return awaitDone(doneMowers, mowersLeft - 1, replyTo);
                 })
                 .build();
 
